a43e2c1de6f0ec507a8728136ce6686ef4feccd5
HelloSrauker <934269360@qq.com>
2021-11-19 00:55:56 +0800

    第二天，添加context,拆分router

--- gee/gee.go
+++ gee/gee.go
@@ -1,38 +1,31 @@
 package gee
 
 import (
-	"fmt"
 	"net/http"
 )
 
 //定义函数类型，使实例化了的此类型函数都能加载
-type HandleFunc func(http.ResponseWriter, *http.Request)
+type HandleFunc func(c *Context)
 
 //用map存路由映射
 type Gee struct {
-	router map[string]HandleFunc
+	router *Router
 }
 
 func New() *Gee {
-	return &Gee{router: map[string]HandleFunc{}}
-}
-
-//将路由存进路由映射
-func (g *Gee) addRouter(method, pattern string, handler HandleFunc) {
-	key := method + "_" + pattern
-	g.router[key] = handler
+	return &Gee{router: NewRouter()}
 }
 
 func (g *Gee) GET(pattern string, handler HandleFunc) {
-	g.addRouter("GET", pattern, handler)
+	g.router.addRouter("GET", pattern, handler)
 }
 
 func (g *Gee) POST(pattern string, handler HandleFunc) {
-	g.addRouter("POST", pattern, handler)
+	g.router.addRouter("POST", pattern, handler)
 }
 
 func (g *Gee) UPDATE(pattern string, handler HandleFunc) {
-	g.addRouter("UPDATE", pattern, handler)
+	g.router.addRouter("UPDATE", pattern, handler)
 }
 
 //gee实现了ServeHTTP方法，可以被实例化为http的Handler
@@ -42,10 +35,6 @@ func (g *Gee) Run(addr string) error {
 
 //根据http req执行注册的路由
 func (g *Gee) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "_" + req.URL.Path
-	if handler, ok := g.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND : %+v", key)
-	}
+	c := NewContext(w, req)
+	g.router.handle(c)
 }
--- main.go
+++ main.go
@@ -8,14 +8,20 @@ import (
 
 func main() {
 	defer fmt.Println("stop running")
-	gee := gee.New()
-	gee.GET("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintln(writer, "hello my friends, this page is GET ")
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	gee.POST("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintln(writer, "hello my friends, this page is POST ")
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
-	fmt.Println("start running")
-	gee.Run(":9999")
+	r.Run(":9999")
 }
