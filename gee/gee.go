10693624a5645a176c07eebbdbcdd4369d6a9e6c
HelloSrauker <934269360@qq.com>
2021-11-21 17:36:09 +0800

    day 4 添加路由分组功能

--- gee/gee.go
+++ gee/gee.go
@@ -1,6 +1,7 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -9,11 +10,51 @@ type HandleFunc func(c *Context)
 
 //用map存路由映射
 type Gee struct {
+	*RouterGroup
 	router *Router
+	groups []*RouterGroup //所有的路由分组
+}
+
+type RouterGroup struct {
+	prefix      string
+	middleWares []HandleFunc // support middleware支持的中间件
+	parent      *RouterGroup // support nesting
+	gee         *Gee         // all groups share a Engine instance
 }
 
 func New() *Gee {
-	return &Gee{router: NewRouter()}
+	gee := &Gee{router: NewRouter()}
+	gee.RouterGroup = &RouterGroup{gee: gee}
+	gee.groups = []*RouterGroup{gee.RouterGroup}
+	return gee
+}
+
+//添加路由分组
+func (r *RouterGroup) Group(prefix string) *RouterGroup {
+	gee := r.gee
+	newGroup := &RouterGroup{
+		prefix: r.prefix + prefix,
+		parent: r,
+		gee:    gee,
+	}
+	gee.groups = append(gee.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandleFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.gee.router.addRouter(method, pattern, handler)
+}
+
+// GET defines the method to add GET request
+func (group *RouterGroup) GET(pattern string, handler HandleFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (group *RouterGroup) POST(pattern string, handler HandleFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 func (g *Gee) GET(pattern string, handler HandleFunc) {
